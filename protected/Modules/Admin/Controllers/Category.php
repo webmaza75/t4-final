be57c844b662652a185e5dd9981771e423229cac
Irina Rabinovich <webmaza75@gmail.com>
2016-06-11 22:23:05 +0300

    final lesson 7 + Админ-панель

---
+++ protected/Modules/Admin/Controllers/Category.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Modules\Admin\Controllers;
+
+use T4\Mvc\Controller;
+use T4\Core\MultiException;
+use App\Models\Category as MCategory;
+
+/**
+ * Class Category - категории новостей
+ * @package App\Modules\Admin\Controllers
+ */
+class Category extends Controller
+{
+    /**
+     * Проверка доступа к админ-панели в раздел Категорий
+     * @param $action
+     * @param array $params
+     * @return bool
+     */
+    protected function access($action, $params = [])
+    {
+        return (!empty($this->app->user) && ($this->app->user->email == 'admin@t4.final'));
+    }
+
+    public function actionDefault()
+    {
+        $this->data->cats = MCategory::findAllTree();
+    }
+
+    public function actionEdit($id = null, $category = null)
+    {
+        $this->data->cats = MCategory::findAllTree();
+
+        if(!empty($id)) {
+            $this->data->category = MCategory::findByPK($id);
+        }
+
+        if(!empty($category)) {
+            try {
+                if (!$category['id']) {
+                    $cat1 = new MCategory();
+                } else {
+                    $cat1 = MCategory::findByPK($category['id']);
+                }
+
+                $cat1->fill($category);
+                $cat1->save();
+                $this->redirect('/admin/category');
+            } catch (MultiException $e) {
+                $this->data->errors = $e;
+                $this->data->category = $category;
+            }
+        }
+    }
+
+    public function actionDelete($id)
+    {
+        $cat = MCategory::findByPK($id);
+        if(!empty($cat)) {
+            $cat->delete();
+        }
+        $this->redirect('/admin/category');
+    }
+}
---
+++ protected/Modules/Admin/Controllers/News.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Modules\Admin\Controllers;
+
+use T4\Mvc\Controller;
+use T4\Core\MultiException;
+use App\Models\Category as MCategory;
+use App\Models\News as MNews;
+
+/**
+ * Class News Новости
+ *
+ * @package App\Modules\Admin\Controllers
+ */
+class News extends Controller
+{
+    /**
+     * Проверка доступа к админ-панели в раздел Новостей
+     * @param $action
+     * @param array $params
+     * @return bool
+     */
+    protected function access($action, $params = [])
+    {
+        return (!empty($this->app->user) && ($this->app->user->email == 'admin@t4.final'));
+    }
+
+    public function actionDefault()
+    {
+        $this->data->news = MNews::findAll();
+    }
+
+    public function actionEdit($id = null, $news = null)
+    {
+        $this->data->cats = MCategory::findAllTree();
+
+        if(!empty($id)) {
+            $this->data->news = MNews::findByPK($id);
+        }
+
+        if(!empty($news)) {
+            try {
+                if (!$news['id']) {
+                    $article = new MNews();
+                } else {
+                    $article  = MNews::findByPK($news['id']);
+                }
+                $article->fill($news);
+                $article->save();
+                $this->redirect('/admin/news');
+            } catch (MultiException $e) {
+                $this->data->errors = $e;
+                $this->data->news = $news;
+            }
+        }
+    }
+
+    public function actionDelete($id)
+    {
+        $article = MNews::findByPK($id);
+        if(!empty($article)) {
+            $article->delete();
+        }
+        $this->redirect('/admin/news');
+    }
+}
