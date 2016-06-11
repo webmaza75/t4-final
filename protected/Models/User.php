97e938b4c64ae52c4b40fb2a08dd847159fa6c03
Irina Rabinovich <webmaza75@gmail.com>
2016-06-11 22:23:52 +0300

    Авторизация / Регистрация

--- protected/Models/User.php
+++ protected/Models/User.php
@@ -4,6 +4,17 @@ namespace App\Models;
 
 use T4\Orm\Model;
 
+/**
+ * Class User Модель пользователь
+ * @package App\Models
+ *
+ * __users таблица
+ * поля:
+ * email string (логин)
+ * password string
+ * firstName string (Имя)
+ * lastName string (Фамилия)
+ */
 class User extends Model
 {
     static  public $schema = [
